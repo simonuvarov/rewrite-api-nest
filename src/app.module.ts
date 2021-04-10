d29c3e2b1d8114f0fd64218cf91ae1faf0450acb
Simon <2471506+simonuvarov@users.noreply.github.com>
2021-04-10 16:04:54 +0300

    remove default routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { IdentityModule } from './identity/identity.module';
 import { PapersModule } from './papers/papers.module';
 
 @Module({
   imports: [PapersModule, IdentityModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
