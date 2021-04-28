24a14028092b7bea25731d57642c6ce0537e903b
Simon <2471506+simonuvarov@users.noreply.github.com>
2021-04-28 13:10:00 +0300

    improve word rule

--- src/engine/rules/informal-words.rule.ts
+++ src/engine/rules/informal-words.rule.ts
@@ -3,24 +3,32 @@ import { BaseRule, RuleProps } from '../base-rule.class';
 import { CRITERIA_TYPE } from '../criteria-type.enum';
 import { InlineIssue } from '../issue.type';
 
-const INFORMAL_WORDS_AVOID_LIST = [
-  'stuff',
-  'awesome',
-  'folks',
-  'kid',
-  'guy',
-  'cop',
-  'ok',
-  'great',
-  'useless',
-  'stupid',
-  'crazy',
-  'dumb',
-];
+interface RULE {
+  replacements?: Array<string>;
+}
 
-const INFORMAL_WORDS_AVOID_MATCH_STRING = `(${INFORMAL_WORDS_AVOID_LIST.join(
-  '|',
-)})`;
+interface WORD_RULE_DICT {
+  [key: string]: RULE;
+}
+
+const INFORMAL_WORDS_AVOID_LIST: WORD_RULE_DICT = {
+  stuff: {},
+  awesome: { replacements: ['great'] },
+  folks: {},
+  kid: {},
+  guy: {},
+  cop: { replacements: ['policeman'] },
+  ok: {},
+  great: {},
+  useless: {},
+  stupid: {},
+  crazy: {},
+  dumb: {},
+};
+
+const INFORMAL_WORDS_AVOID_MATCH_STRING = `(${Object.keys(
+  INFORMAL_WORDS_AVOID_LIST,
+).join('|')})`;
 
 export class InformalWordsRule extends BaseRule {
   get affects(): CRITERIA_TYPE {
@@ -30,19 +38,18 @@ export class InformalWordsRule extends BaseRule {
     const matches = props.parsedBody.match(INFORMAL_WORDS_AVOID_MATCH_STRING);
     const matchesJson = matches.json({ offset: true });
 
-    const issues: Array<InlineIssue> = matchesJson.map(
-      (m) =>
-        ({
-          id: uuid(),
-          isInline: true,
-          affects: this.affects,
-          message:
-            'You are asked to write an academic essay. Avoid using informal words to make your language as formal and academic as possible.',
-          shortMessage: `Informal word: ${m.terms[0].text}`,
-          offset: m.terms[0].offset.start,
-          length: m.terms[0].offset.length,
-        } as InlineIssue),
-    );
+    const issues: Array<InlineIssue> = matchesJson.map((m) => {
+      const issue: InlineIssue = {
+        id: uuid(),
+        isInline: true,
+        affects: this.affects,
+        message: `You are asked to write an academic essay. Avoid using informal words such as ${m.terms[0].text} to make your language as formal and academic as possible.`,
+        shortMessage: `Informal word: ${m.terms[0].text}`,
+        offset: m.terms[0].offset.start,
+        length: m.terms[0].offset.length,
+      };
+      return issue;
+    });
 
     this.issues.push(...issues);
 
