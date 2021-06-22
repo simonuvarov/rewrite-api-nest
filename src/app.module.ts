538223b6f48456e41b7e966156950e80d09e8ac5
Simon <2471506+simonuvarov@users.noreply.github.com>
2021-06-22 17:52:53 +0300

    add config parser

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import * as Joi from 'joi';
 import { IdentityModule } from './identity/identity.module';
 import { NotificationsModule } from './notifications/notifications.module';
 import { PapersModule } from './papers/papers.module';
@@ -10,6 +12,19 @@ import { PapersModule } from './papers/papers.module';
     IdentityModule,
     EventEmitterModule.forRoot(),
     NotificationsModule,
+    //TODO: extract into separate module
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string()
+          .regex(/postgresql:\/\//)
+          .required(),
+        SECRET: Joi.string().required(),
+        LT_URL: Joi.string()
+          .regex(/https?:\/\//)
+          .required(),
+        POSTMARK_API_KEY: Joi.string().required(),
+      }),
+    }),
   ],
 })
 export class AppModule {}
